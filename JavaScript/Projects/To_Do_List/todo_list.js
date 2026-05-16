81460171e436014ce4aefcc0b75484f3dbc02471
Rayan <mrayan2525@gmail.com>
2026-05-16 17:44:45 +0100

    Implemented CSS and grid format in html

--- JavaScript/Projects/To_Do_List/todo_list.js
+++ JavaScript/Projects/To_Do_List/todo_list.js
@@ -11,13 +11,14 @@ function renderToDoList() {
     for (let i=0; i< todoList.length; i++) {
         const todoObject = todoList[i]
         const {name, dueDate} = todoObject
-        const html = `<p>
-        ${name} ${dueDate}
+        const html =
+        `<div>${name}</div>
+        <div>${dueDate}</div>
         <button onclick="
-        todoList.splice(${i}, 1); // Removes from list
-        renderToDoList();
-        ">Delete</button>
-        </p>`
+            todoList.splice(${i}, 1);
+            renderToDoList();
+        " class="delete-todo-button">Delete</button>
+        `
         todoListHTML += html
     }
 
