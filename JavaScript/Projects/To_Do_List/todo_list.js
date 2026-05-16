fc0fc13ea841ed655a3122e26e55363787cefd7a
Rayan <mrayan2525@gmail.com>
2026-05-16 17:06:35 +0100

    Implemented date input

--- JavaScript/Projects/To_Do_List/todo_list.js
+++ JavaScript/Projects/To_Do_List/todo_list.js
@@ -1,4 +1,6 @@
-const todoList = [];
+const todoList = [
+    {name: 'make dinner', dueDate: '2026-12-1'},
+    {name: 'wash dishes', dueDate: '2026-10-4'}]
 
 renderToDoList();
 
@@ -7,11 +9,17 @@ function renderToDoList() {
     let todoListHTML = '';
 
     for (let i=0; i< todoList.length; i++) {
-        const todo = todoList[i]
-        const html = `<p>${todo}</p>`
+        const todoObject = todoList[i]
+        const {name, dueDate} = todoObject
+        const html = `<p>
+        ${name} ${dueDate}
+        <button onclick="
+        todoList.splice(${i}, 1); // Removes from list
+        renderToDoList();
+        ">Delete</button>
+        </p>`
         todoListHTML += html
     }
-    console.log(todoListHTML)
 
     // Adds todoList into div
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
@@ -22,8 +30,13 @@ function addToDo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
 
-    todoList.push(name);
-    console.log(todoList);
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+
+    todoList.push({
+        name: name,
+        dueDate: dueDate
+    });
 
     inputElement.value = ''; // Rest text box to empty
 
