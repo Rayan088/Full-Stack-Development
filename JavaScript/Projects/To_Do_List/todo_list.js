37559af85b2ff793823fd686cf6ab13c949dd128
Rayan <mrayan2525@gmail.com>
2026-05-16 16:14:13 +0100

    ToDo list html

---
+++ JavaScript/Projects/To_Do_List/todo_list.js
@@ -0,0 +1,11 @@
+const todoList = [];
+
+function addToDo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+
+    todoList.push(name);
+    console.log(todoList);
+
+    inputElement.value = ""; // Makes text box empty
+}
