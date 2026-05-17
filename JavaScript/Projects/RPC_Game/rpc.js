5835393b703bc68b663c46b1abab7fe1b207aa4f
Rayan <mrayan2525@gmail.com>
2026-05-17 16:44:46 +0100

    Implemented autoPlay feature

--- JavaScript/Projects/RPC_Game/rpc.js
+++ JavaScript/Projects/RPC_Game/rpc.js
@@ -7,6 +7,24 @@ let score = JSON.parse(localStorage.getItem("score")) || {
 
 updateScoreElement();
 
+// Function to autoplay game
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay() {
+    if (!isAutoPlaying) {
+        intervalId = setInterval(function() {
+        const playerMove = pickComputerMove()
+        playGame(playerMove);
+    }, 1000);
+    isAutoPlaying = true;
+
+    } else {
+        clearInterval(intervalId);
+        isAutoPlaying = false;
+    }
+}
+
 // Function for deciding outcome
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
