ce0b43dc492aa5bce19be9dcae85967239005280
Rayan <mrayan2525@gmail.com>
2026-05-17 16:56:02 +0100

    forEach function to replace traditional for loop

--- JavaScript/Projects/To_Do_List/todo_list.js
+++ JavaScript/Projects/To_Do_List/todo_list.js
@@ -8,19 +8,21 @@ renderToDoList();
 function renderToDoList() {
     let todoListHTML = '';
 
-    for (let i=0; i< todoList.length; i++) {
-        const todoObject = todoList[i]
+    todoList.forEach(
+        function (todoObject, index) {
+        const todoObject = todoList[index]
         const {name, dueDate} = todoObject
         const html =
         `<div>${name}</div>
         <div>${dueDate}</div>
         <button onclick="
-            todoList.splice(${i}, 1);
+            todoList.splice(${index}, 1);
             renderToDoList();
         " class="delete-todo-button">Delete</button>
         `
         todoListHTML += html
-    }
+        }
+    )
 
     // Adds todoList into div
     document.querySelector('.js-todo-list').innerHTML = todoListHTML;
