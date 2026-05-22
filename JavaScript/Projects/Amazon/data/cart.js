69dab08aa06550d347cfc32757639d7481ae96d7
Rayan <mrayan2525@gmail.com>
2026-05-22 11:45:31 +0100

    save data and generate html for delivery options

--- JavaScript/Projects/Amazon/data/cart.js
+++ JavaScript/Projects/Amazon/data/cart.js
@@ -4,15 +4,18 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart) {
     cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', // Normalising data as prodcuctID is primary key
-    quantity: 2}, {
+    quantity: 2,
+    deliveryOptionId: '1'}, 
+    {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1}];
+    quantity: 1,
+    deliveryOptionId: '2'}];
 }
 
 // Saving to storage
 function saveToStorage() {
     localStorage.setItem('cart', JSON.stringify(cart));
-}
+} 
 
 // Add to cart function
 export function addToCart(productId) {
@@ -30,7 +33,8 @@ export function addToCart(productId) {
     } else {
         cart.push({
         productId: productId,
-        quantity: 1
+        quantity: 1,
+        deliveryOptionId: '1'
     });
     };
 
