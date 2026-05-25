119889af3c0ce05026578457ee9c0d7d8e538257
Rayan <mrayan2525@gmail.com>
2026-05-25 20:13:50 +0100

    Moved code into classes and declared public and private properties and methods

---
+++ JavaScript/Projects/Amazon/data/cart-class.js
@@ -0,0 +1,89 @@
+class Cart {
+    cartItems = undefined; // public property - accessible anywhere
+    #localStorageKey = undefined; // # makes attribute private - only accessible in the class
+
+    constructor(localStorageKey) { //Method that auto runs when object is generated
+        this.#localStorageKey = localStorageKey
+        this.#loadFromStorage();
+    }
+
+    #loadFromStorage() { // Methods can also be private
+        this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey)); // this gives the outer object
+
+        if (!this.cartItems) {
+            this.cartItems = [{
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', // Normalising data as prodcuctID is primary key
+            quantity: 2,
+            deliveryOptionId: '1'}, 
+            {
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1,
+            deliveryOptionId: '2'}];
+        }
+    }
+    
+    // Saving to storage
+    saveToStorage() {
+            localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems))}
+
+    // Add to cart method
+    addToCart(productId) {
+        let matchingItem;
+
+        this.cartItems.forEach((cartItem) => {
+            if (productId === cartItem.productId) {
+                matchingItem = cartItem;
+            }
+        });
+
+        // Increment total if already in cart
+        if (matchingItem) {
+            matchingItem.quantity ++;
+        } else {
+            this.cartItems.push({
+            productId: productId, 
+            quantity: 1,
+            deliveryOptionId: '1'
+        });
+        };
+
+        this.saveToStorage();
+    }
+
+    // Removing from cart method
+    removeFromCart(productId) {
+        const newCart = [];
+
+        this.cartItems.forEach((cartItem) => {
+            if (cartItem.productId !== productId) {
+                newCart.push(cartItem);
+            }
+        });
+
+        this.cartItems = newCart;
+
+        this.saveToStorage();
+    }
+
+    updateDeliveryOption(productId, deliveryOptionId) {
+        let matchingItem;
+
+        this.cartItems.forEach((cartItem) => {
+            if (productId === cartItem.productId) {
+                matchingItem = cartItem;
+            }
+        });
+
+        matchingItem.deliveryOptionId = deliveryOptionId;
+
+        this.saveToStorage();
+    }
+}
+
+// Class instances
+const cart = new Cart('cart-oop'); // generates new object using class
+const businessCart = new Cart('cart-business');
+
+console.log(cart);
+console.log(businessCart);
+console.log(businessCart instanceof Cart); // Checks if instance is of class
