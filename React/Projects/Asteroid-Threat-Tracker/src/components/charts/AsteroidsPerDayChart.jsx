344e5de9b9dfd699dc455848c9bc6790501c3c73
Rayan <mrayan2525@gmail.com>
2026-06-05 20:20:34 +0100

    Added bar chart colours

--- React/Projects/Asteroid-Threat-Tracker/src/components/charts/AsteroidsPerDayChart.jsx
+++ React/Projects/Asteroid-Threat-Tracker/src/components/charts/AsteroidsPerDayChart.jsx
@@ -11,11 +11,22 @@ const AsteoidsPerDayChart = ({data}) => {
     return (
         <ResponsiveContainer width="100%" height={300}>
             <BarChart data={(data)}>
-                <XAxis dataKey="shortDate" />
-                <YAxis />
+
+                <defs>
+                    <linearGradient id="barGradient" x1="0" y1="1" x2="0" y2="0">
+                        <stop offset="0%" stopColor="#2C1A70" />
+                        <stop offset="100%" stopColor="#500aef" />
+                    </linearGradient>
+                </defs>
+
+                <XAxis dataKey="shortDate" stroke='#fff'/>
+                <YAxis stroke='#fff'/>
                 <Tooltip />
 
-                <Bar dataKey="count"/>
+                <Bar
+                    dataKey="count"
+                    fill="url(#barGradient)"/>
+                    
             </BarChart>
         </ResponsiveContainer>
     )
