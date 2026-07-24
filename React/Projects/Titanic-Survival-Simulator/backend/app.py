661cfd85ca67ce5bab9e31379be86f75430bad60
Rayan <mrayan2525@gmail.com>
2026-07-24 18:01:12 +0100

    Created flask routes for survival statistics

--- React/Projects/Titanic-Survival-Simulator/backend/app.py
+++ React/Projects/Titanic-Survival-Simulator/backend/app.py
@@ -3,11 +3,14 @@ from config import Config
 from database.db import db
 
 from models.passenger import Passenger
+from routes.survival_routes import survival_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+
+app.register_blueprint(survival_bp)
 
 with app.app_context():
     db.create_all()
---
+++ React/Projects/Titanic-Survival-Simulator/backend/routes/survival_routes.py
@@ -0,0 +1,64 @@
+from flask import Blueprint, jsonify
+from services.survival_queries import (get_survival_by_class, get_survival_by_age, get_survival_by_gender, get_survival_by_port)
+
+survival_bp = Blueprint("survival", __name__)
+
+@survival_bp.route("/api/insights/class", methods=["GET"])
+def survival_by_class():
+    results = get_survival_by_class()
+
+    data = []
+    for row in results:
+        data.append({
+            "class": row[0],
+            "survival_percentage": float(row[1])
+        })
+
+    return jsonify(data)
+
+# Route for survival by class
+
+@survival_bp.route("/api/insights/gender", methods=["GET"])
+def survival_by_gender():
+    results = get_survival_by_gender()
+
+    data = []
+    for row in results:
+        data.append({
+            "gender": row[0],
+            "survival_percentage": float(row[1])
+        })
+
+    return jsonify(data)
+
+# Route for survival by gender
+
+@survival_bp.route("/api/insights/age", methods=["GET"])
+def survival_by_age():
+    results = get_survival_by_age()
+
+    data = []
+    for row in results:
+        data.append({
+            "age_group": row[0],
+            "survival_percentage": float(row[1])
+        })
+
+    return jsonify(data)
+
+# Route for survival by age
+
+@survival_bp.route("/api/insights/port", methods=["GET"])
+def survival_by_port():
+    results = get_survival_by_port()
+
+    data = []
+    for row in results:
+        data.append({
+            "port": row[0],
+            "survival_percentage": float(row[1])
+        })
+
+    return jsonify(data)
+
+# Route for survival by embarkment port
