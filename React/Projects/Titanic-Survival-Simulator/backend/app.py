bc841b5c2e71c764094ff6daf748f473d3677ac5
Rayan <mrayan2525@gmail.com>
2026-07-24 14:58:43 +0100

    Cleaned data using pandas

--- React/Projects/Titanic-Survival-Simulator/backend/app.py
+++ React/Projects/Titanic-Survival-Simulator/backend/app.py
@@ -2,10 +2,16 @@ from flask import Flask
 from config import Config
 from database.db import db
 
+from models.passenger import Passenger
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ React/Projects/Titanic-Survival-Simulator/backend/database/seed_database.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+from app import app
+from database.db import db
+from models.passenger import Passenger
+
+def seed_database():
+    df = pd.read_csv("data/titanic.csv")
+
+    with app.app_context():
+        for _, row in df.iterrows():
+
+            age = None
+            if not pd.isna(row["age"]):
+                age = int(row["age"])
+            # Age to be None if empty
+
+            embarked = None
+            if not pd.isna(row["embarked"]):
+                embarked_lookup = {
+                    "S": "Southampton",
+                    "C": "Cherbourg",
+                    "Q": "Queenstown"
+                }
+
+                embarked = embarked_lookup.get(
+                    row["embarked"],
+                    row["embarked"]
+                )
+            # Embarked location as full instead of initial
+
+
+            family_size = None
+
+            if not pd.isna(row["sibsp"]) and not pd.isna(row["parch"]):
+                family_size = int(row["sibsp"] + row["parch"] + 1)
+            # Full family size
+
+            passenger = Passenger(
+                name=row["name"],
+                age=age,
+                sex=row["sex"],
+                passenger_class=int(row["pclass"]),
+                survived=int(row["survived"]),
+                embarked=embarked,
+                family_size=family_size,
+            )
+
+            db.session.add(passenger)
+
+        db.session.commit()
+
+        print("Database seeded successfully")
+
+# Cleaning table rows
+
+if __name__ == "__main__":
+    seed_database()
