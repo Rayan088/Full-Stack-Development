10bd4b1887c33063207e8a7ee8e45ca33380252c
Rayan <mrayan2525@gmail.com>
2026-07-28 10:45:11 +0100

    Implemented summary cards in main UI

--- React/Projects/Titanic-Survival-Simulator/frontend/vite-project/src/components/summaryCards.jsx
+++ React/Projects/Titanic-Survival-Simulator/frontend/vite-project/src/components/summaryCards.jsx
@@ -1,7 +1,25 @@
 function SummaryCards() {
     return (
-        <div>
-            Summary Cards
+        <div className="summary-cards">
+            <div className="summary-card">
+                <div className="summary-number">2,224</div>
+                <div className="summary-label">TOTAL PASSENGERS</div>
+            </div>
+
+            <div className="summary-card">
+                <div className="summary-number">1,502</div>
+                <div className="summary-label">DID NOT SURVIVE</div>
+            </div>
+
+            <div className="summary-card">
+                <div className="summary-number">711</div>
+                <div className="summary-label">SURVIVED</div>
+            </div>
+
+            <div className="summary-card">
+                <div className="summary-number">32%</div>
+                <div className="summary-label">OVERRAL SURVIVAL RATE</div>
+            </div>
         </div>
     );
 }
